4a69ea2c3904fa99d80c12f8b0142468da9fbbbc
Jackson <z5397791@ad.unsw.edu.au>
2023-02-02 16:02:42 +1100

    Add model component within presentation control to render the single model

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 
 function Model(props) {
   const { scene } = useGLTF("/bmw.glb");
-  return <primitive object={scene} {...props} />
+  return <primitive object={scene} scale={0.01} {...props} />;
 }
 
 function App() {
   return (
-    <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{ "position": "absolute" }}>
+    <Canvas
+      dpr={[1, 2]}
+      shadows
+      camera={{ fov: 45 }}
+      style={{ position: "absolute" }}
+    >
       <color attach="background" args={["#101010"]} />
+      <PresentationControls
+        speed={1.5}
+        global
+        zoom={0.5}
+        polar={[-0.1, Math.PI / 4]}
+      >
+        <Stage environment={null}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
     </Canvas>
   );
 }
