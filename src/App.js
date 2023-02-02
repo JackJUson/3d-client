887513869f7f7948110fc395439faca5d5ac2402
Jackson <z5397791@ad.unsw.edu.au>
2023-02-02 15:45:52 +1100

    Add model function which takes scene property out of bmw glb file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("/bmw.glb");
+}
 
 function App() {
   return (
